23bd23b732086571971808b2370801c32154b815
Henry Bi <henrybi@ripl.com>
2020-01-15 16:41:57 -0800

    add ability to license images

--- lib/shutterstock-ruby/images.rb
+++ lib/shutterstock-ruby/images.rb
@@ -6,9 +6,19 @@ module ShutterstockRuby
       JSON.parse(get('/images/search', { query: query }.merge(options)))
     end
 
+    def license(image_id, subscription_id, size, body_metadata, options = {})
+      body = { images: [ { image_id: image_id }.merge(body_metadata)] }.to_json
+
+      JSON.parse(post('/images/licenses', body, {subscription_id: subscription_id, size: size }), options)
+    end
+
     class << self
       def search(query, options = {})
         client.search(query, options)
+      end
+
+      def license(image_id, subscription_id, size, body_metadata = {}, options = {})
+        client.license(image_id, subscription_id, size, body_metadata, options)
       end
     end
   end
