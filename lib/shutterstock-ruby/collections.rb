109901a1d3202a654d09a0fe6db88a08ab1c3fbb
David R <david@ripl.com>
2020-03-16 21:47:19 -0700

    more collection endpoints

--- lib/shutterstock-ruby/collections.rb
+++ lib/shutterstock-ruby/collections.rb
@@ -6,9 +6,17 @@ module ShutterstockRuby
       JSON.parse(get('/images/collections/featured', options))
     end
 
+    def list_collection_items(collection_id, options = {})
+      JSON.parse(get("/images/collections/featured/#{collection_id}/items", options))
+    end
+
     class << self
       def list_all_featured_collections(options = {})
         client.list_all_featured_collections(options)
+      end
+
+      def list_collection_items(collection_id, options = {})
+        client.list_collection_items(collection_id, options)
       end
 
     end
